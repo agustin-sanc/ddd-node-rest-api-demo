b2d9d3f1ccd1828f75ba0667d88eb8c7e01f055f
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-29 20:22:29 -0300

    Change user's constructor params

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -1,9 +1,12 @@
 export default class User {
-  constructor(
-    private readonly emailAddress: string,
-    private readonly firstName: string,
-    private readonly lastName: string
-  ) {
+  constructor(params: {
+    emailAddress: string,
+    firstName: string,
+    lastName: string
+  })
+   {
+    const { emailAddress, firstName, lastName } = params;
+
     if (!emailAddress) throw new Error('emailAddress must be defined');
     if (!firstName) throw new Error('firstName must be defined');
     if (!lastName) throw new Error('lastName must be defined');
