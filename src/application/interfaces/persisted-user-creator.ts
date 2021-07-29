70255bfd85602592500c5746ea084f8e464ffa08
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-29 20:26:27 -0300

    Move user.ts to domain/entities folder

--- src/application/interfaces/persisted-user-creator.ts
+++ src/application/interfaces/persisted-user-creator.ts
@@ -1,4 +1,4 @@
-import User from "../../domain/user";
+import User from "../../domain/entities/user";
 
 export default interface PersistedUserCreator {
   createPersistedUser(user: User): Promise<void>;
