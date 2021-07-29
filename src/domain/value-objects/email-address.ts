c7cf2000ced5b137388c31f198ba8410f6ae911e
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-29 20:46:42 -0300

    Add Email Address value object

---
+++ src/domain/value-objects/email-address.ts
@@ -0,0 +1,44 @@
+export default class EmailAddress {
+  constructor(
+    private readonly value: string
+  ) {
+    if(!value) throw new Error('Email address value must be defined')
+
+    if(!this.isValidValueFormat())
+      throw new Error('Email address value is invalid')
+  }
+
+  private isValidValueFormat(): boolean {
+    const emailRegex =
+      /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+    if (!this.value) return false;
+
+    if (this.value.length > 254) return false;
+
+    const valid = emailRegex.test(this.value);
+    if (!valid) return false;
+
+    const parts = this.value.split('@');
+    if (parts[0].length > 64) return false;
+
+    const domainParts = parts[1].split('.');
+
+    if (
+      domainParts.some(function (part) {
+        return part.length > 63;
+      })
+    )
+      return false;
+
+    return true;
+  }
+
+  public getValue(): string {
+    return this.value;
+  }
+
+  public equals(emailAddress: EmailAddress): boolean {
+    return this.value === emailAddress.getValue();
+  }
+}
