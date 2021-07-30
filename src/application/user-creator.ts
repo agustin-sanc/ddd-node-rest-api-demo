0beb6c362e2fef348fc3ea706eef6e782e2ccfa3
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-30 02:51:02 -0300

    Add logic to invalidate sign up of users with already taken email addresses

--- src/application/user-creator.ts
+++ src/application/user-creator.ts
@@ -3,13 +3,27 @@ import PersistedUserCreator from "./interfaces/persisted-user-creator";
 import EmailAddress from "../domain/value-objects/email-address";
 import Password from "../domain/value-objects/password";
 import ID from "../domain/value-objects/id";
+import PersistedUserFinderByEmailAddress from "./interfaces/persisted-user-finder-by-email-address";
+import {UserTypes} from "../domain/enums/user-types";
 
 export default class UserCreator {
-  constructor(
-    private readonly persistedUserCreator: PersistedUserCreator
-  ) {
-    if(!persistedUserCreator)
+  private readonly persistedUserCreator: PersistedUserCreator;
+  private readonly persistedUserFinderByEmailAddress: PersistedUserFinderByEmailAddress;
+
+  constructor(params: {
+    persistedUserCreator: PersistedUserCreator,
+    persistedUserFinderByEmailAddress: PersistedUserFinderByEmailAddress
+  }) {
+    if(!params.persistedUserCreator)
       throw new Error('persistedUserCreator must be defined')
+
+    if(!params.persistedUserFinderByEmailAddress)
+      throw new Error('persistedUserFinderByEmailAddress must be defined')
+
+    this.persistedUserCreator = params.persistedUserCreator;
+
+    this.persistedUserFinderByEmailAddress =
+      params.persistedUserFinderByEmailAddress;
   }
 
   public async createUser(params: {
@@ -17,6 +31,24 @@ export default class UserCreator {
     password: Password,
     type: string
   }): Promise<User> {
+    const foundUserWithSameEmailAddress =
+      await this.persistedUserFinderByEmailAddress
+        .findPersistedUserByEmailAddress(
+          params.emailAddress
+        );
+
+    if (foundUserWithSameEmailAddress) {
+      throw new Error(
+        `Conflict. Email address is already taken.`
+      );
+    }
+
+    if (params.type === UserTypes.ADMIN_USER) {
+      throw new Error(
+        `Validation. Admin user only can be created directly into database.`
+      )
+    }
+
     const createdUser = new User({
       id: ID.create(),
       emailAddress: params.emailAddress,
