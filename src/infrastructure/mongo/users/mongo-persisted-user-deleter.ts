153ce0a80868bb32b07d4a7da9178706855fbb6e
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-30 01:40:12 -0300

    Add mongo persisted user deleter service at infrastructure

---
+++ src/infrastructure/mongo/users/mongo-persisted-user-deleter.ts
@@ -0,0 +1,20 @@
+import { UserMongoDocumentModel } from "./user-mongo-document-model";
+import PersistedUserDeleter from "../../../application/interfaces/persisted-user-deleter";
+import ID from "../../../domain/value-objects/id";
+
+export default class MongoPersistedUserCreator
+  implements PersistedUserDeleter {
+  public async deletePersistedUser(id: ID): Promise<void> {
+    await UserMongoDocumentModel
+      .deleteOne({
+        _id: id.getValue()
+      })
+      .catch(error => {
+        console.error(error);
+
+        throw new Error(
+          `Error deleting user with id ${ id.getValue() } from MongoDB`
+        )
+      })
+  }
+}
