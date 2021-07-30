f94e5ecfa97c5a4325d769663383339666b079eb
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-30 01:26:01 -0300

    Add find user by id endpoint controller

---
+++ src/infrastructure/rest-api/endpoints/users/controllers/find-user-by-id-endpoint-controller.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from "express";
+import User from "../../../../../domain/entities/user";
+import UserFinderById from "../../../../../application/user-finder-by-id";
+import getRawUserObject from "../../../../../domain/mappers/get-raw-user-object";
+import ID from "../../../../../domain/value-objects/id";
+
+export default class findUserByIdEndpointController {
+  constructor(
+    private readonly userFinderById: UserFinderById
+  ) {
+    if (!userFinderById)
+      throw new Error('userFinderById must be defined');
+  }
+
+  public async execute(params: {
+    request: Request,
+    response: Response
+  }) : Promise<Response> {
+    const { request, response } = params;
+
+    try {
+      this.validateRequestParams(request.query);
+    } catch(error) {
+      console.error(error);
+
+      return response
+        .status(400)
+        .send({
+          error: error.message
+        })
+    }
+
+    let user: User;
+
+    try {
+      user = await this.userFinderById.findUserById(
+        new ID(request.query.id)
+      );
+    } catch(error) {
+      console.error(error);
+
+      if (error.message.includes("doesn't exist"))
+        return response
+          .status(404)
+          .send({
+            error: error.message
+          })
+
+      return response
+        .status(500)
+        .send({
+          error: 'Internal server error',
+          description: error.message
+        })
+    }
+
+    return response
+      .status(200)
+      .send({
+        message: 'User obtained successfully',
+        user: getRawUserObject(user)
+      })
+  }
+
+  private validateRequestParams(params: any): void {
+    if (!params.id)
+      throw new Error('id parameter must be defined');
+  }
+}
