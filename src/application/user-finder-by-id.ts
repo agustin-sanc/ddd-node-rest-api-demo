a0cb7acc06e8d7f7e286d91739fd6f8439379a16
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-30 01:24:46 -0300

    Add user finder by id service in application layer

---
+++ src/application/user-finder-by-id.ts
@@ -0,0 +1,27 @@
+import User from "../domain/entities/user";
+import ID from "../domain/value-objects/id";
+import PersistedUserFinderById from "./interfaces/persisted-user-finder-by-id";
+
+export default class UserFinderById {
+  constructor(
+    private readonly persistedUserFinderById: PersistedUserFinderById
+  ) {
+    if (!persistedUserFinderById)
+      throw new Error('persistedUserFinderById must be defined');
+  }
+
+  public async findUserById(id: ID): Promise<User> {
+    return await this.persistedUserFinderById
+      .findPersistedUserById(id)
+      .catch(error => {
+        console.error(error);
+
+        if (error.message.includes("doesn't exist"))
+          throw error;
+
+        throw new Error(
+          `Error finding persisted user with id = ${ id.getValue() }`
+        );
+      });
+  }
+}
