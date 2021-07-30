479a49a18e95c6dc334c9288f5f43957b4755e56
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-30 01:27:58 -0300

    Reformat minor code issues

--- src/infrastructure/builders/users-application-services/build-user-creator-application-service.ts
+++ src/infrastructure/builders/users-application-services/build-user-creator-application-service.ts
@@ -4,5 +4,6 @@ import UserCreator from "../../../application/user-creator";
 export default function buildUserCreatorApplicationService()
   : UserCreator {
   const persistedUserCreator = new MongoPersistedUserCreator();
+
   return new UserCreator(persistedUserCreator);
 }
