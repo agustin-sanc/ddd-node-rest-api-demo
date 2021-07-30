b4d91ef2addc0850bbdd58f8dbf3da768e4d2f9b
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-29 21:56:59 -0300

    Add build-user-creator-application-service.ts

---
+++ src/infrastructure/builders/users-application-services/build-user-creator-application-service.ts
@@ -0,0 +1,8 @@
+import MongoPersistedUserCreator from "../../mongo/users/mongo-persisted-user-creator";
+import UserCreator from "../../../application/user-creator";
+
+export default function buildUserCreatorApplicationService()
+  : UserCreator {
+  const persistedUserCreator = new MongoPersistedUserCreator();
+  return new UserCreator(persistedUserCreator);
+}
