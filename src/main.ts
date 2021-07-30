a86fa4bc6e711cc914feb04f3795b4f298b6d9be
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-29 21:00:37 -0300

    Add call to createHttpServerWithRESTAPI

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 import setupEnvironmentVariables from "./infrastructure/setup-environment-variables";
+import createHttpServerWithRESTAPI from "./infrastructure/rest-api/create-http-server-with-rest-api";
 
 setupEnvironmentVariables();
+createHttpServerWithRESTAPI();
