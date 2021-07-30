0811b26358be8f46c0b943e12169fc782dd3d1f6
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-07-30 02:52:19 -0300

    Update user creator instantiation at builder

--- src/infrastructure/builders/users-application-services/build-user-creator-application-service.ts
+++ src/infrastructure/builders/users-application-services/build-user-creator-application-service.ts
@@ -1,9 +1,16 @@
 import MongoPersistedUserCreator from "../../mongo/users/mongo-persisted-user-creator";
 import UserCreator from "../../../application/user-creator";
+import MongoPersistedUsersFinder from "../../mongo/users/mongo-persisted-users-finder";
 
 export default function buildUserCreatorApplicationService()
   : UserCreator {
   const persistedUserCreator = new MongoPersistedUserCreator();
 
-  return new UserCreator(persistedUserCreator);
+  const persistedUserFinderByEmailAddress =
+    new MongoPersistedUsersFinder();
+
+  return new UserCreator({
+    persistedUserCreator,
+    persistedUserFinderByEmailAddress
+  });
 }
