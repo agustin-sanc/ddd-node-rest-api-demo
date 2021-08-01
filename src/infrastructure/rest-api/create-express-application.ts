59c21d9bf58a9be3b787add33dc2110ecf4af54e
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-08-01 18:11:25 -0300

    Add custom error handling middleware

--- src/infrastructure/rest-api/create-express-application.ts
+++ src/infrastructure/rest-api/create-express-application.ts
@@ -2,12 +2,15 @@ import { Application } from 'express';
 import * as express from 'express';
 import setupCommonMiddlewares from './middlewares/setup-common-middlewares';
 import setupEndpoints from './endpoints/setup-endpoints';
+import errorHandlingMiddleware from "./middlewares/errors/error-handling-middleware";
 
 export default function createExpressApplication(): Application {
   const expressApplication = express();
 
   setupCommonMiddlewares(expressApplication);
   setupEndpoints(expressApplication);
+
+  expressApplication.use(errorHandlingMiddleware);
 
   return expressApplication;
 }
