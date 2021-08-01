ba57a97a4d1ef78d2838bb88ced3b1a256e34f87
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-08-01 18:07:15 -0300

    Design improvement of find users controller

---
+++ src/infrastructure/rest-api/endpoints/users/find-users-controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import getRawUserObject from "../../../../domain/mappers/get-raw-user-object";
+import buildUsersFinderApplicationService from "../../../builders/build-users-finder-application-service";
+import User from "../../../../domain/entities/user";
+
+export default async function findUsersController(
+  request: Request,
+  response: Response,
+  next: any
+): Promise<void> {
+  let users: User[] = [];
+
+  try {
+    const usersFinder = buildUsersFinderApplicationService();
+    users = await usersFinder.findUsers();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
+  response
+    .status(200)
+    .send({
+      message: 'Users obtained successfully',
+      users: users.map(user => {
+        return getRawUserObject(user);
+      })
+    });
+
+  return;
+}
