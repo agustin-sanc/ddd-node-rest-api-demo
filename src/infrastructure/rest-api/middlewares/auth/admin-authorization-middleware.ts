76b9d08b8106102838967658322a19c036ccdc99
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-08-01 18:06:13 -0300

    Add admin authorization middleware

---
+++ src/infrastructure/rest-api/middlewares/auth/admin-authorization-middleware.ts
@@ -0,0 +1,25 @@
+import {UserTypes} from "../../../../domain/enums/user-types";
+import User from "../../../../domain/entities/user";
+import ForbiddenError from "../errors/forbidden-error";
+import { Request, Response } from 'express';
+
+export default function adminAuthorizationMiddleware(
+  request: Request,
+  response: Response,
+  next: any
+): void {
+  const user: User = request.user;
+
+  const allowedUserTypes = [
+    UserTypes.ADMIN_USER
+  ];
+
+  for (const allowedUserType of allowedUserTypes) {
+    if (user.hasType(allowedUserType)) {
+      next();
+      return;
+    }
+  }
+
+  next(new ForbiddenError());
+}
