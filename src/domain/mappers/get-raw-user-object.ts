9a853b736ceabaf2d178e69d2cb50fe8f836976e
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-08-01 21:42:32 -0300

    Remove password field

--- src/domain/mappers/get-raw-user-object.ts
+++ src/domain/mappers/get-raw-user-object.ts
@@ -3,13 +3,11 @@ import User from "../entities/user";
 export default function getRawUserObject(user: User): {
   id: string,
   emailAddress: string,
-  password: string,
   type: string
 } {
   return {
     id: user.getId().getValue(),
     emailAddress: user.getEmailAddress().getValue(),
-    password: user.getPassword().getValue(),
     type: user.getType()
   }
 }
