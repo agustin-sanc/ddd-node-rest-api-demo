e602e9b9d3103bdea80b24078180bb3bcc3ebae3
José Agustín Sánchez <js.agustin.sz@gmail.com>
2021-08-01 21:42:45 -0300

    Fix minor issue

--- src/infrastructure/rest-api/create-express-application.ts
+++ src/infrastructure/rest-api/create-express-application.ts
@@ -1,14 +1,14 @@
 import { Application } from 'express';
 import * as express from 'express';
 import setupCommonMiddlewares from './middlewares/setup-common-middlewares';
-import setupEndpoints from './endpoints/setup-endpoints';
+import setupRouters from './endpoints/setup-routers';
 import errorHandlingMiddleware from "./middlewares/errors/error-handling-middleware";
 
 export default function createExpressApplication(): Application {
   const expressApplication = express();
 
   setupCommonMiddlewares(expressApplication);
-  setupEndpoints(expressApplication);
+  setupRouters(expressApplication);
 
   expressApplication.use(errorHandlingMiddleware);
 
